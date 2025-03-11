a18e0ba2778995e25aabc38c4b71dd06c04e5392
jsdevuz <jsdevuz.me@gmail.com>
2025-03-11 18:02:13 +0500

    gg

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
